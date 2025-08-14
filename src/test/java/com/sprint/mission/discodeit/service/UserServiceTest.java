7fcc3c19bee1a3a9e951fde0deb5fa07302a23ff
joongwon <anjoongwon517@gmail.com>
2025-08-14 10:09:28 +0900

    feat: 단위 테스트 - user_create_success/fail

---
+++ src/test/java/com/sprint/mission/discodeit/service/UserServiceTest.java
@@ -0,0 +1,102 @@
+package com.sprint.mission.discodeit.service;
+
+import com.sprint.mission.discodeit.dto.data.UserDto;
+import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
+import com.sprint.mission.discodeit.entity.User;
+import com.sprint.mission.discodeit.exception.user.UserEmailAlreadyExistsException;
+import com.sprint.mission.discodeit.mapper.UserMapper;
+import com.sprint.mission.discodeit.repository.UserRepository;
+import com.sprint.mission.discodeit.service.basic.BasicUserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(SpringExtension.class)
+class UserServiceTest {
+
+    @InjectMocks
+    private BasicUserService userService;
+
+    @Mock
+    private UserRepository userRepository;
+
+    @Mock
+    private UserMapper userMapper;
+
+    @Test
+    @DisplayName("사용자 가입 성공")
+    void user_create_success() {
+        // given
+        UserCreateRequest request= new UserCreateRequest(
+                "username",
+                "email@codeit.com",
+                "password"
+        );
+        // 중복 검사는 모두 통과한다 치고
+        when(userRepository.existsByUsername(anyString())).thenReturn(false);
+        when(userRepository.existsByEmail(anyString())).thenReturn(false);
+
+        when(userMapper.toDto(any(User.class))).thenReturn(new UserDto(
+                UUID.randomUUID(),
+                "username",
+                "email@codeit.com",
+                null,
+                true
+        ));
+
+        // when
+        // 회원 가입
+        // 질문) 여기서는 프로필 이미지 없다치고, 만약 프로필 이미지도 검증하고 싶으면 다른 테스트로 빼야하는거 맞나요?
+        UserDto result = userService.create(request, Optional.empty());
+
+        // then
+        assertNotNull(result);
+        verify(userRepository, times(1)).save(any(User.class));
+    }
+
+    @Test
+    @DisplayName("중복 이메일로 인한 회원가입 실패")
+    void user_create_fail_duplicate_email() {
+        // given
+        UserCreateRequest request= new UserCreateRequest(
+                "username",
+                "email@codeit.com",
+                "password"
+        );
+        when(userRepository.existsByUsername(anyString())).thenReturn(false);
+        // 중복된 이메일 존재
+        when(userRepository.existsByEmail(anyString())).thenReturn(true);
+
+        when(userMapper.toDto(any(User.class))).thenReturn(new UserDto(
+                UUID.randomUUID(),
+                "username",
+                "email@codeit.com",
+                null,
+                true
+        ));
+
+        // when & then
+        assertThrows(UserEmailAlreadyExistsException.class, () -> userService.create(request, Optional.empty()));
+        verify(userRepository, never()).save(any(User.class));
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
