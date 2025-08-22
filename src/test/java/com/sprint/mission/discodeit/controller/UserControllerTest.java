0394743d4f7ba53c2ab0ffc91e077b0a9725390a
joongwon <anjoongwon517@gmail.com>
2025-08-22 16:51:28 +0900

    feat(test): UserController update Slice Test

--- src/test/java/com/sprint/mission/discodeit/controller/UserControllerTest.java
+++ src/test/java/com/sprint/mission/discodeit/controller/UserControllerTest.java
@@ -2,31 +2,33 @@ package com.sprint.mission.discodeit.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sprint.mission.discodeit.dto.data.UserDto;
-import com.sprint.mission.discodeit.dto.request.BinaryContentCreateRequest;
 import com.sprint.mission.discodeit.dto.request.UserCreateRequest;
+import com.sprint.mission.discodeit.dto.request.UserUpdateRequest;
+import com.sprint.mission.discodeit.exception.ErrorCode;
 import com.sprint.mission.discodeit.exception.GlobalExceptionHandler;
+import com.sprint.mission.discodeit.exception.user.UserNotFoundException;
 import com.sprint.mission.discodeit.service.UserService;
 import com.sprint.mission.discodeit.service.UserStatusService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springdoc.core.service.RequestBodyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -46,7 +48,7 @@ class UserControllerTest {
 
     @Test
     @DisplayName("유저 생성 성공 - 201 Created, 생성 유저 정보 반환")
-    void createUser_success() throws Exception {
+    void createUserSuccess() throws Exception {
         // given
         UUID userId = UUID.randomUUID();
         UserCreateRequest request = new UserCreateRequest("username", "email@codeit.com", "password");
@@ -78,7 +80,7 @@ class UserControllerTest {
 
     @Test
     @DisplayName("유저 생성 실패 - 필수 필드(username) 누락으로 404 반환")
-    void createUser_failure_missing_name_Field() throws Exception {
+    void createUserFailureMissingNameField() throws Exception {
         // given
         UserCreateRequest request = new UserCreateRequest(null, "email@codeit.com", "password");
 
@@ -99,5 +101,71 @@ class UserControllerTest {
 
         // then
         actions.andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("user 업데이트 성공 - 200 Ok, user update 정보 반환")
+    void updateUserSuccess() throws Exception {
+        // given
+        UUID userId = UUID.randomUUID();
+        UserUpdateRequest userUpdateRequest = new UserUpdateRequest("newUsernama", "newEmail@codeit.com", "newPassword");
+        UserDto userDto = new UserDto(userId, userUpdateRequest.newUsername(), userUpdateRequest.newEmail(), null, true);
+        given(userService.update(eq(userId), any(UserUpdateRequest.class), eq(Optional.empty()))).willReturn(userDto);
+
+        MockMultipartFile userPart = new MockMultipartFile(
+                "userUpdateRequest",
+                "",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsBytes(userUpdateRequest)
+        );
+
+        // when
+        ResultActions actions = mvc.perform(
+                multipart("/api/users/{userId}", userId)
+                        .file(userPart)
+                        .with(request -> {
+                            request.setMethod("PATCH");
+                            return request;
+                        })
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        actions.andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(userId.toString()))
+                .andExpect(jsonPath("$.username").value(userUpdateRequest.newUsername()))
+                .andExpect(jsonPath("$.email").value(userUpdateRequest.newEmail()));
+    }
+
+    @Test
+    @DisplayName("user 업데이트 실패 - 잘못된 파라미터(userId)로 인해 user 찾을 수 없음")
+    void updateUserFailCasedByWrongUserId() throws Exception {
+        // given
+        UUID wrongUserId = UUID.randomUUID();
+        UserUpdateRequest userUpdateRequest =
+                new UserUpdateRequest("newName", "newEmail@codeit.com", "newPassword");
+
+        given(userService.update(eq(wrongUserId), any(UserUpdateRequest.class), any()))
+                .willThrow(new UserNotFoundException(Map.of("userId", wrongUserId.toString())));
+
+        MockMultipartFile userPart = new MockMultipartFile(
+                "userUpdateRequest",
+                "",
+                MediaType.APPLICATION_JSON_VALUE,
+                objectMapper.writeValueAsBytes(userUpdateRequest)
+        );
+
+        // when
+        ResultActions actions = mvc.perform(
+                multipart("/api/users/{userId}", wrongUserId)
+                        .file(userPart)
+                        .with(req -> { req.setMethod("PATCH"); return req; })
+                        .contentType(MediaType.MULTIPART_FORM_DATA)
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+
+        // then
+        actions.andExpect(status().isNotFound());
     }
 }
