31675ca74ed03efa93059f80a50d410a5362794c
joongwon <anjoongwon517@gmail.com>
2025-10-18 18:31:06 +0900

    refresh token으로 access token 재발급하는 API 구현

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,23 +1,23 @@
 package com.sprint.mission.discodeit.controller;
 
 import com.sprint.mission.discodeit.controller.api.AuthApi;
+import com.sprint.mission.discodeit.dto.data.JwtDto;
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.RoleUpdateRequest;
 import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
+import com.sprint.mission.discodeit.security.jwt.JwtTokenProvider;
 import com.sprint.mission.discodeit.service.AuthService;
 import com.sprint.mission.discodeit.service.UserService;
+
 import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.web.csrf.CsrfToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -25,35 +25,55 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController implements AuthApi {
 
-  private final AuthService authService;
-  private final UserService userService;
+    private final AuthService authService;
+    private final UserService userService;
 
-  @GetMapping("csrf-token")
-  public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
-    log.debug("CSRF 토큰 요청");
-    log.trace("CSRF 토큰: {}", csrfToken.getToken());
-    return ResponseEntity
-        .status(HttpStatus.NO_CONTENT)
-        .build();
-  }
+    private final JwtTokenProvider jwtTokenProvider;
 
-  @GetMapping("me")
-  public ResponseEntity<UserDto> me(@AuthenticationPrincipal DiscodeitUserDetails userDetails) {
-    log.info("내 정보 조회 요청");
-    UUID userId = userDetails.getUserDto().id();
-    UserDto userDto = userService.find(userId);
-    return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(userDto);
-  }
+    @GetMapping("csrf-token")
+    public ResponseEntity<Void> getCsrfToken(CsrfToken csrfToken) {
+        log.debug("CSRF 토큰 요청");
+        log.trace("CSRF 토큰: {}", csrfToken.getToken());
+        return ResponseEntity
+                .status(HttpStatus.NO_CONTENT)
+                .build();
+    }
 
-  @PutMapping("role")
-  public ResponseEntity<UserDto> updateRole(@RequestBody RoleUpdateRequest request) {
-    log.info("권한 수정 요청");
-    UserDto userDto = authService.updateRole(request);
+    @GetMapping("me")
+    public ResponseEntity<UserDto> me(@AuthenticationPrincipal DiscodeitUserDetails userDetails) {
+        log.info("내 정보 조회 요청");
+        UUID userId = userDetails.getUserDto().id();
+        UserDto userDto = userService.find(userId);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userDto);
+    }
 
-    return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(userDto);
-  }
+    @PutMapping("role")
+    public ResponseEntity<UserDto> updateRole(@RequestBody RoleUpdateRequest request) {
+        log.info("권한 수정 요청");
+        UserDto userDto = authService.updateRole(request);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(userDto);
+    }
+
+    @PostMapping("refresh")
+    public ResponseEntity<?> renewAccessTokenFromRefreshToken(@CookieValue("REFRESH_TOKEN") String refreshTokenValue) {
+        log.info("# Access Token 재발급 요청");
+
+        if (!refreshTokenValue.isEmpty() || jwtTokenProvider.validateToken(refreshTokenValue)) {
+            JwtDto jwtDto = authService.renewToken(refreshTokenValue);
+            log.debug("# Access Token 재발급 성공");
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(jwtDto);
+        } else {
+            log.error("# Access Token 재발급 실패");
+            return ResponseEntity
+                    .status(HttpStatus.UNAUTHORIZED)
+                    .build();
+        }
+    }
 }
