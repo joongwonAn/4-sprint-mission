3dbc670559165ff62c0fa2bf470d2b73f8da2586
joongwon <anjoongwon517@gmail.com>
2025-10-18 18:32:28 +0900

    refresh token으로 access token 재발급

--- src/main/java/com/sprint/mission/discodeit/service/AuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/AuthService.java
@@ -1,11 +1,14 @@
 package com.sprint.mission.discodeit.service;
 
+import com.sprint.mission.discodeit.dto.data.JwtDto;
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.RoleUpdateRequest;
 
 public interface AuthService {
 
-  UserDto updateRole(RoleUpdateRequest request);
+    UserDto updateRole(RoleUpdateRequest request);
 
-  UserDto updateRoleInternal(RoleUpdateRequest request);
+    UserDto updateRoleInternal(RoleUpdateRequest request);
+
+    JwtDto renewToken(String refreshTokenValue);
 }
--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicAuthService.java
@@ -1,15 +1,22 @@
 package com.sprint.mission.discodeit.service.basic;
 
+import com.sprint.mission.discodeit.dto.data.JwtDto;
 import com.sprint.mission.discodeit.dto.data.UserDto;
 import com.sprint.mission.discodeit.dto.request.RoleUpdateRequest;
+import com.sprint.mission.discodeit.entity.RefreshToken;
 import com.sprint.mission.discodeit.entity.Role;
 import com.sprint.mission.discodeit.entity.User;
 import com.sprint.mission.discodeit.exception.user.UserNotFoundException;
 import com.sprint.mission.discodeit.mapper.UserMapper;
+import com.sprint.mission.discodeit.repository.RefreshTokenRepository;
 import com.sprint.mission.discodeit.repository.UserRepository;
 import com.sprint.mission.discodeit.security.SessionManager;
+import com.sprint.mission.discodeit.security.jwt.JwtTokenProvider;
 import com.sprint.mission.discodeit.service.AuthService;
+
+import java.util.Map;
 import java.util.UUID;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -21,31 +28,63 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class BasicAuthService implements AuthService {
 
-  private final UserRepository userRepository;
-  private final UserMapper userMapper;
-  private final SessionManager sessionManager;
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final SessionManager sessionManager;
 
-  @PreAuthorize("hasRole('ADMIN')")
-  @Transactional
-  @Override
-  public UserDto updateRole(RoleUpdateRequest request) {
-    return updateRoleInternal(request);
-  }
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final RefreshTokenService refreshTokenService;
 
-  @Transactional
-  @Override
-  public UserDto updateRoleInternal(RoleUpdateRequest request) {
-    UUID userId = request.userId();
-    User user = userRepository.findById(userId)
-        .orElseThrow(() -> UserNotFoundException.withId(userId));
+    @PreAuthorize("hasRole('ADMIN')")
+    @Transactional
+    @Override
+    public UserDto updateRole(RoleUpdateRequest request) {
+        return updateRoleInternal(request);
+    }
 
-    Role newRole = request.newRole();
-    user.updateRole(newRole);
+    @Transactional
+    @Override
+    public UserDto updateRoleInternal(RoleUpdateRequest request) {
+        UUID userId = request.userId();
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> UserNotFoundException.withId(userId));
 
-    sessionManager.invalidateSessionsByUserId(userId);
+        Role newRole = request.newRole();
+        user.updateRole(newRole);
 
-    return userMapper.toDto(user);
-  }
+        sessionManager.invalidateSessionsByUserId(userId);
 
+        return userMapper.toDto(user);
+    }
 
+    @Override
+    @Transactional
+    public JwtDto renewToken(String refreshTokenValue) {
+        if (!jwtTokenProvider.validateToken(refreshTokenValue)) {
+            throw new RuntimeException("Invalid refresh token");
+        }
+
+        RefreshToken refreshToken = refreshTokenRepository.findByToken(refreshTokenValue)
+                .orElseThrow(() -> new RuntimeException("등록되지 않은 리프레시 토큰"));
+
+        if (refreshToken.getRotated()) {
+            throw new RuntimeException("이미 사용된 리프레시 토큰");
+        }
+
+        Map<String, Object> claims = jwtTokenProvider.getClaims(refreshTokenValue);
+        UUID userId = UUID.fromString(claims.get("sub").toString());
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> UserNotFoundException.withId(userId));
+
+        String newAccessToken = jwtTokenProvider.renewAccessToken(refreshToken.getToken());
+        refreshToken.invalidate();
+        refreshTokenRepository.save(refreshToken);
+
+        RefreshToken newRefreshToken = refreshTokenService.saveRefreshToken(userId);
+
+        log.info("# Refresh Token과 Access Token 재발급 완료, refreshToken = {}, accessToken = {}", newRefreshToken, newAccessToken);
+        return new JwtDto(userMapper.toDto(user), newAccessToken);
+    }
 }
