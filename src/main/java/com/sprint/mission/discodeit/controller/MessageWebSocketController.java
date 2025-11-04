e7a01cfe7d8272b239e3c65ada96cf0496958ffe
joongwon <anjoongwon517@gmail.com>
2025-11-04 15:32:31 +0900

    STOMP 요청 처리 컨트롤러

---
+++ src/main/java/com/sprint/mission/discodeit/controller/MessageWebSocketController.java
@@ -0,0 +1,23 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.dto.data.MessageDto;
+import com.sprint.mission.discodeit.dto.request.MessageCreateRequest;
+import com.sprint.mission.discodeit.service.MessageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+@Slf4j
+public class MessageWebSocketController {
+    private final MessageService messageService;
+
+    @MessageMapping("/message") // STOMP의 /pub/message 요청 처리
+    public void handleMessage(MessageCreateRequest request) {
+        log.info("# 첨부 파일 없는 메시지 생성 요청: request={}", request);
+        MessageDto createdMessage = messageService.create(request, null);
+        log.debug("# 첨부 파일 없는 메시지 생성 응답: {}", createdMessage);
+    }
+}
