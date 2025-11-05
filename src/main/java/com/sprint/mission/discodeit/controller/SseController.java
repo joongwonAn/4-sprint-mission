add84f7bc61c1bb694603b48fc2b001139e41e56
joongwon <anjoongwon517@gmail.com>
2025-11-05 15:56:16 +0900

    feat: GET /api/sse로 SSE 연결 엔드포인트 구성

---
+++ src/main/java/com/sprint/mission/discodeit/controller/SseController.java
@@ -0,0 +1,25 @@
+package com.sprint.mission.discodeit.controller;
+
+import com.sprint.mission.discodeit.service.SseService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/sse")
+@RequiredArgsConstructor
+@Slf4j
+public class SseController {
+    private final SseService sseService;
+
+    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public SseEmitter connect(@RequestParam UUID receiverId,
+                              @RequestHeader(value = "Last-Event-ID", required = false) UUID lastEventId) {
+        log.debug("# SseEmitter connect 요청, receiverId={}, lastEventId={}", receiverId, lastEventId);
+        return sseService.connect(receiverId, lastEventId);
+    }
+}
