290376354febb62b43fbe86218dda1e94a807b3d
joongwon <anjoongwon517@gmail.com>
2025-11-06 22:24:10 +0900

    refactor: @EnableRetry 위치 변경

--- src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
+++ src/main/java/com/sprint/mission/discodeit/DiscodeitApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
-@EnableRetry // Spring Retry 활성화
 public class DiscodeitApplication {
 
     public static void main(String[] args) {
