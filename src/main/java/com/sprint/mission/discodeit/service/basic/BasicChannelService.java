0b71df7ea7c3ddbc7420dfd6d35ae93a8fa4c4a1
joongwon <anjoongwon517@gmail.com>
2025-11-06 14:12:11 +0900

    feat: 채널 갱신 이벤트(ChannelUpdateEvent) 발행

--- src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
+++ src/main/java/com/sprint/mission/discodeit/service/basic/BasicChannelService.java
@@ -7,6 +7,7 @@ import com.sprint.mission.discodeit.dto.request.PublicChannelUpdateRequest;
 import com.sprint.mission.discodeit.entity.Channel;
 import com.sprint.mission.discodeit.entity.ChannelType;
 import com.sprint.mission.discodeit.entity.ReadStatus;
+import com.sprint.mission.discodeit.event.ChannelUpdateEvent;
 import com.sprint.mission.discodeit.exception.channel.ChannelNotFoundException;
 import com.sprint.mission.discodeit.exception.channel.PrivateChannelUpdateException;
 import com.sprint.mission.discodeit.mapper.ChannelMapper;
@@ -24,6 +25,7 @@ import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cache.annotation.Caching;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -40,6 +42,7 @@ public class BasicChannelService implements ChannelService {
     private final MessageRepository messageRepository;
     private final UserRepository userRepository;
     private final ChannelMapper channelMapper;
+    private final ApplicationEventPublisher publisher;
 
     @PreAuthorize("hasRole('CHANNEL_MANAGER')")
     @Transactional
@@ -49,13 +52,18 @@ public class BasicChannelService implements ChannelService {
             evict = {@CacheEvict(value = "channelCacheByUserId", allEntries = true)}
     )
     public ChannelDto create(PublicChannelCreateRequest request) {
-        log.debug("채널 생성 시작: {}", request);
+        log.debug("public 채널 생성 시작: {}", request);
         String name = request.name();
         String description = request.description();
         Channel channel = new Channel(ChannelType.PUBLIC, name, description);
 
-        channelRepository.save(channel);
-        log.info("채널 생성 완료: id={}, name={}", channel.getId(), channel.getName());
+        Channel saved = channelRepository.save(channel);
+        ChannelDto channelDto = channelMapper.toDto(saved);
+        log.info("public 채널 생성 완료: id={}, name={}", channel.getId(), channel.getName());
+
+        log.debug("# 채널 갱신 SSE 이벤트 전송(created public channel), payload={}", channelDto);
+        publisher.publishEvent(new ChannelUpdateEvent(channelDto, "created"));
+
         return channelMapper.toDto(channel);
     }
 
@@ -66,16 +74,21 @@ public class BasicChannelService implements ChannelService {
             evict = {@CacheEvict(value = "channelCacheByUserId", allEntries = true)}
     )
     public ChannelDto create(PrivateChannelCreateRequest request) {
-        log.debug("채널 생성 시작: {}", request);
+        log.debug("private 채널 생성 시작: {}", request);
         Channel channel = new Channel(ChannelType.PRIVATE, null, null);
-        channelRepository.save(channel);
+        Channel saved = channelRepository.save(channel);
+        ChannelDto channelDto = channelMapper.toDto(saved);
 
         List<ReadStatus> readStatuses = userRepository.findAllById(request.participantIds()).stream()
                 .map(user -> new ReadStatus(user, channel, channel.getCreatedAt()))
                 .toList();
         readStatusRepository.saveAll(readStatuses);
 
-        log.info("채널 생성 완료: id={}, name={}", channel.getId(), channel.getName());
+        log.info("private 채널 생성 완료: id={}, name={}", channel.getId(), channel.getName());
+
+        log.debug("# 채널 갱신 SSE 이벤트 전송(created private channel), payload={}", channelDto);
+        publisher.publishEvent(new ChannelUpdateEvent(channelDto, "created"));
+
         return channelMapper.toDto(channel);
     }
 
@@ -121,6 +134,11 @@ public class BasicChannelService implements ChannelService {
         }
         channel.update(newName, newDescription);
         log.info("채널 수정 완료: id={}, name={}", channelId, channel.getName());
+
+        ChannelDto channelDto = channelMapper.toDto(channel);
+        log.debug("# 채널 갱신 SSE 이벤트 전송(updated), payload={}", channelDto);
+        publisher.publishEvent(new ChannelUpdateEvent(channelDto, "updated"));
+
         return channelMapper.toDto(channel);
     }
 
@@ -133,14 +151,17 @@ public class BasicChannelService implements ChannelService {
     })
     public void delete(UUID channelId) {
         log.debug("채널 삭제 시작: id={}", channelId);
-        if (!channelRepository.existsById(channelId)) {
-            throw ChannelNotFoundException.withId(channelId);
-        }
+        Channel channel = channelRepository.findById(channelId)
+                .orElseThrow(() -> ChannelNotFoundException.withId(channelId));
+        ChannelDto channelDto = channelMapper.toDto(channel);
 
         messageRepository.deleteAllByChannelId(channelId);
         readStatusRepository.deleteAllByChannelId(channelId);
 
         channelRepository.deleteById(channelId);
         log.info("채널 삭제 완료: id={}", channelId);
+
+        log.debug("# 채널 갱신 SSE 이벤트 전송(deleted), payload={}", channelDto);
+        publisher.publishEvent(new ChannelUpdateEvent(channelDto, "deleted"));
     }
 }
