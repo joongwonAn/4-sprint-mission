f8e703127be00b336017061a5eca89cd817165b1
joongwon <anjoongwon517@gmail.com>
2025-11-06 14:12:21 +0900

    feat: 채널 갱신 이벤트(ChannelUpdateEvent)

---
+++ src/main/java/com/sprint/mission/discodeit/event/ChannelUpdateEvent.java
@@ -0,0 +1,9 @@
+package com.sprint.mission.discodeit.event;
+
+import com.sprint.mission.discodeit.dto.data.ChannelDto;
+
+public record ChannelUpdateEvent(
+        ChannelDto channelDto,
+        String type
+) {
+}
