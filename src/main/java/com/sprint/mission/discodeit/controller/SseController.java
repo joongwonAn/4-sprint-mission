e0a44ede70e0f01f62fae6feceb6e859b45d614c
joongwon <anjoongwon517@gmail.com>
2025-11-06 10:39:25 +0900

    fix: 파라미터 매핑 오류 해결

--- src/main/java/com/sprint/mission/discodeit/controller/SseController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/SseController.java
@@ -1,9 +1,11 @@
 package com.sprint.mission.discodeit.controller;
 
+import com.sprint.mission.discodeit.security.DiscodeitUserDetails;
 import com.sprint.mission.discodeit.service.SseService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
@@ -17,8 +19,9 @@ public class SseController {
     private final SseService sseService;
 
     @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public SseEmitter connect(@RequestParam UUID receiverId,
+    public SseEmitter connect(@AuthenticationPrincipal DiscodeitUserDetails user,
                               @RequestHeader(value = "Last-Event-ID", required = false) UUID lastEventId) {
+        UUID receiverId = user.getUserDto().id();
         log.debug("# SseEmitter connect 요청, receiverId={}, lastEventId={}", receiverId, lastEventId);
         return sseService.connect(receiverId, lastEventId);
     }
